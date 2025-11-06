187850028ca4305aad2d57443be86ba288ef76f3
Tokiwadai-Railgun <arthur.scheidbach@live.fr>
2025-11-06 17:38:10 +0100

    make clean close

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { Server } from 'http'
 
 const app = new Hono()
 
@@ -7,9 +8,27 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-serve({
+
+
+const server = serve({
   fetch: app.fetch,
   port: 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
+})
+
+process.on("SIGINT", () => {
+    server.close()
+    console.log("Closing");
+    process.exit(0)
+})
+
+process.on('SIGTERM', () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
 })
